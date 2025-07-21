41c67d3097cfb8747a5a32ee45a5748b594fb6af
usmankiani256 <usmankiani256@gmail.com>
2025-07-21 15:59:20 +0500

    feat: add airport and flight search API endpoints with mock data

---
+++ src/api/searchAirport.ts
@@ -0,0 +1,118 @@
+// import apiClient from './ApiClient';
+import { AirportSearchParams, AirportSearchResponse } from '@/interfaces';
+
+export const searchAirport = async (
+  params: AirportSearchParams,
+): Promise<AirportSearchResponse> => {
+  console.log('API:searchAirport', { params, response: mockData });
+  return mockData;
+  // try {
+  //   const response = await apiClient.get('/searchAirport', { params });
+  //   return response.data;
+  // } catch (error) {
+  //   console.error(error);
+  //   throw error;
+  // }
+};
+
+/**
+ * ------------------------------------------------------------
+ * MOCK DATA
+ * ------------------------------------------------------------
+ */
+const mockData: AirportSearchResponse = {
+  status: true,
+  timestamp: 1691008938320,
+  data: [
+    {
+      presentation: {
+        title: 'New York',
+        suggestionTitle: 'New York (Any)',
+        subtitle: 'United States',
+      },
+      navigation: {
+        entityId: '27537542',
+        entityType: 'CITY',
+        localizedName: 'New York',
+        relevantFlightParams: {
+          skyId: 'NYCA',
+          entityId: '27537542',
+          flightPlaceType: 'CITY',
+          localizedName: 'New York',
+        },
+      },
+    },
+    {
+      presentation: {
+        title: 'New York Newark',
+        suggestionTitle: 'New York Newark (EWR)',
+        subtitle: 'United States',
+      },
+      navigation: {
+        entityId: '95565059',
+        entityType: 'AIRPORT',
+        localizedName: 'New York Newark',
+        relevantFlightParams: {
+          skyId: 'EWR',
+          entityId: '95565059',
+          flightPlaceType: 'AIRPORT',
+          localizedName: 'New York Newark',
+        },
+      },
+    },
+    {
+      presentation: {
+        title: 'New York John F. Kennedy',
+        suggestionTitle: 'New York John F. Kennedy (JFK)',
+        subtitle: 'United States',
+      },
+      navigation: {
+        entityId: '95565058',
+        entityType: 'AIRPORT',
+        localizedName: 'New York John F. Kennedy',
+        relevantFlightParams: {
+          skyId: 'JFK',
+          entityId: '95565058',
+          flightPlaceType: 'AIRPORT',
+          localizedName: 'New York John F. Kennedy',
+        },
+      },
+    },
+    {
+      presentation: {
+        title: 'New York LaGuardia',
+        suggestionTitle: 'New York LaGuardia (LGA)',
+        subtitle: 'United States',
+      },
+      navigation: {
+        entityId: '95565057',
+        entityType: 'AIRPORT',
+        localizedName: 'New York LaGuardia',
+        relevantFlightParams: {
+          skyId: 'LGA',
+          entityId: '95565057',
+          flightPlaceType: 'AIRPORT',
+          localizedName: 'New York LaGuardia',
+        },
+      },
+    },
+    {
+      presentation: {
+        title: 'Stewart International',
+        suggestionTitle: 'Stewart International (SWF)',
+        subtitle: 'United States',
+      },
+      navigation: {
+        entityId: '95566280',
+        entityType: 'AIRPORT',
+        localizedName: 'Stewart International',
+        relevantFlightParams: {
+          skyId: 'SWF',
+          entityId: '95566280',
+          flightPlaceType: 'AIRPORT',
+          localizedName: 'Stewart International',
+        },
+      },
+    },
+  ],
+};
